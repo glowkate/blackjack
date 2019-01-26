fc313e3e2e8a4e1cc5440378a57d033975390adf
unknown <andrew.brownbill@gmail.com>
2019-01-26 17:31:47 -0500

    Added enums to blackjack (1).py and a card class to improve readability

--- blackjack (1).py
+++ blackjack (1).py
@@ -1,34 +1,103 @@
 import random
 import unittest
 import time
+from enum import Enum,auto
 
-ACTIVE = 1
-INACTIVE = 2
-BUST = 3
+
+class State(Enum):
+	ACTIVE = auto()
+	INACTIVE = auto()
+	BUST = auto()
+
+class Turn(Enum):
+	HIT = auto()
+	STOP = auto()
+
+class Suit(Enum):
+	HEART = auto()
+	DIAMOND = auto()
+	SPADE = auto()
+	CLUB = auto()
+
+class Value(Enum):
+	ACE = auto()
+	TWO = auto()
+	THREE = auto()
+	FOUR = auto()
+	FIVE = auto()
+	SIX = auto()
+	SEVEN = auto()
+	EIGHT = auto()
+	NINE = auto()
+	TEN = auto()
+	JACK = auto()
+	QUEEN = auto()
+	KING = auto()
+
+SUIT_TO_TXT = {}
+SUIT_TO_TXT[Suit.HEART] = "hearts"
+SUIT_TO_TXT[Suit.DIAMOND] = "diamonds"
+SUIT_TO_TXT[Suit.SPADE] = "spades"
+SUIT_TO_TXT[Suit.CLUB] = "clubs"
+
+VALUE_TO_POINTS = {}
+VALUE_TO_POINTS[Value.ACE] = 11
+VALUE_TO_POINTS[Value.TWO] = 2
+VALUE_TO_POINTS[Value.THREE] = 3
+VALUE_TO_POINTS[Value.FOUR] = 4
+VALUE_TO_POINTS[Value.FIVE] = 5
+VALUE_TO_POINTS[Value.SIX] = 6
+VALUE_TO_POINTS[Value.SEVEN] = 7
+VALUE_TO_POINTS[Value.EIGHT] = 8
+VALUE_TO_POINTS[Value.NINE] = 9
+VALUE_TO_POINTS[Value.TEN] = 10
+VALUE_TO_POINTS[Value.JACK] = 10
+VALUE_TO_POINTS[Value.QUEEN] = 10
+VALUE_TO_POINTS[Value.KING] = 10
+
+VALUE_TO_TXT = {}
+VALUE_TO_TXT[Value.ACE] = "ace"
+VALUE_TO_TXT[Value.TWO] = "two"
+VALUE_TO_TXT[Value.THREE] = "three"
+VALUE_TO_TXT[Value.FOUR] = "four"
+VALUE_TO_TXT[Value.FIVE] = "five"
+VALUE_TO_TXT[Value.SIX] = "six"
+VALUE_TO_TXT[Value.SEVEN] = "seven"
+VALUE_TO_TXT[Value.EIGHT] = "eight"
+VALUE_TO_TXT[Value.NINE] = "nine"
+VALUE_TO_TXT[Value.TEN] = "ten"
+VALUE_TO_TXT[Value.JACK] = "jack"
+VALUE_TO_TXT[Value.QUEEN] = "queen"
+VALUE_TO_TXT[Value.KING] = "king"
+
+class Card():
+	def __init__(self, suit, value):
+		self.suit = suit
+		self.value = value
 
 class Player():
 	#initilise the player
 	def __init__(self, user, name):
-		self.user = user #set to 0 if human, otherwise the value is the value that the ai stops at
+		self.__user = user #set to 0 if human, otherwise the value is the value that the ai stops at
+		self.__point_cap = user #for readability
 		self.name = name
 		self.deck = []
-		self.points = 0
-		self.state = ACTIVE
+		self.__points = 0
+		self.state = State.ACTIVE
 	
 	#give the player a card, award points, and if nessisary, set the player to innactive
 	def recive_card(self, card):
 		self.deck.append(card)
 		if card[1] == 11 or card[1] == 12 or card[1] == 13:
-			self.points += 10
+			self.__points += 10
 		else:
-			self.points += card[1]
-		if self.points > 21:
-			self.state = BUST
-
-
+			self.__points += card[1]
+		if self.__points > 21:
+			self.state = State.BUST
 			
+	#changes the state of the player to innactive
 	def stop(self):
-		self.state = INACTIVE
+		self.state = State.INACTIVE
 		
 
 	#Human and ai turns. Returns 0 if the user is continuing, 1 if the player has stoped.
@@ -36,54 +105,41 @@ class Player():
 		print(self.name + "'s turn.")
 		if self.user == 0:
 			print("You have the following hand:")
-			for i in self.deck:
-				print("A " + read_card(i))
-			print("Your hand has a value of " + str(self.points) + " points.")
-			hit = get_menu(["hit", "stop"], "Would you like to HIT, or would you like to STOP?", "Please select a valid option")
-			return hit
+			read_hand(self.hand)
+			print("Your hand has a value of " + str(self.__points) + " points.")
+			ans = get_menu(["hit", "stop"], "Would you like to HIT, or would you like to STOP?", "Please select a valid option")
+			if ans == 0:
+				turn = Turn.STOP
+			else:
+				turn = Turn.HIT
 		else:
-			if self.points >= self.user:
+			if self.__points >= self.__point_cap:
 				print(self.name + " has stop.")
-				return 1
+				turn = Turn.STOP
 			else:
 				print(self.name + " has drawn a card.")
-				return 0
-			
+				turn = Turn.HIT
+		return turn
 
-#creates a deck and shuffles it, then returns the deck
-def make_deck():
+
+#creates a deck and shuffles it, then returns the deck. Decks in the number of decks being shuffled
+def make_deck(decks):
 	deck = []
-	for s in range(4):
-		for v in range(13):
-			deck.append((s + 1, v + 1))
+	for i in decks:
+		for s in Suit:
+			for v in Value:
+				new_card = Card(s, v)
+				deck.append(new_card)
 	random.shuffle(deck)
 	return deck
 
-#creates and returns a string of the card	
+#creates and returns a string of the card
 def read_card(card):
-	if card[0] == 1:
-		suit = "Hearts"
-	elif card[0] == 2:
-		suit = "Dimonds"
-	elif card[0] == 3:
-		suit = "Clubs"
-	elif card[0] == 4:
-		suit = "Spades"
-	else:
-		suit = "Jevils"
-	
-	if card[1] == 1:
-		value = "Ace"
-	elif card[1] == 11:
-		value = "Jack"
-	elif card[1] == 12:
-		value = "Queen"
-	elif card[1] == 13:
-		value = "King"
-	else:
-		value = str(card[1])
-	
-	word = value + " of " + suit
+	suit = card[0]
+	value = card[1]
+	suit_txt = SUIT_TO_TXT.get(suit)
+	value_txt = VALUE_TO_TXT.get(value)
+	word = value_txt.capitalize() + " of " + suit.capitalize()
 	return word
 
 def read_hand(hand):
@@ -104,22 +160,22 @@ def get_menu(valid, prompt, error):
 
 
 #takes in a list of player objects returns the object of the player that won, returns None if nobody wins
-def do_game(active):
-	if len(active) == 0:
+def do_game(players):
+	if len(players) == 0:
 		print("There are no players to play!")
 		return None
 	else:
 		game = True
-		deck = make_deck()
+		deck = make_deck(1) #makes the deck
 		innactive = [] #when someone stops, they go from the active list into the innactive list. When someone busts, they are removed from the active list and are NOT added to the innactive list 
+		active = players
 		for p in active:
 			for i in range(2):
 				card = deck.pop()
 				p.recive_card(card)
 		while game == True:
-
 			active_2 = active
-			print("ding")
+			#print("ding")
 			for p in active_2:
 				print(p.name)
 				print(p.state)
@@ -147,8 +203,7 @@ def do_game(active):
 							print(winners[i].name + ", "),
 						print("and " + winners[length - 1] + ".")
 					game = False
-
-			print(active)		
+			print(active)
 			for p in active:
 				turn = p.get_turn()
 				if turn == 0:
@@ -160,15 +215,11 @@ def do_game(active):
 						print(p.name + " has bust.")
 				elif turn == 1:
 					p.stop()
-
-			
 		return winners
-							
-					
-					
 
 
-#players is a list of players who stoped. Returns the winers
+
+#players is a list of players who are able to win. Returns the winers.
 def find_winner(players):
 	point = 0
 	winners = []
@@ -181,15 +232,15 @@ def find_winner(players):
 	return winners
 
 
-cpu = Player(16, "CPU 1")
-you = Player(0, "Kate")
-do_game([cpu, you])
+#cpu = Player(16, "CPU 1")
+#you = Player(0, "Kate")
+#do_game([cpu, you])
 
-
-
-
-
-
-
-                        
-                        
+#print(State.ACTIVE)
+#print(repr(State.ACTIVE))
+#print(type(State.ACTIVE))
+#print(isinstance(State.ACTIVE, State))
+#print("==============")
+#print(State(1))
+#print(State(2))
+#print(State(3))
